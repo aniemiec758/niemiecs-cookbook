67095dfada082941ea34f60e168ddaf4d2ef5114
Anthony Niemiec <aniemiec758@gmail.com>
2019-02-23 21:06:29 -0500

    using commit.sh to commit commit.sh

--- commit.sh
+++ commit.sh
@@ -3,7 +3,14 @@
 # because I'm lazy
 
 git add -A
+
+# getting the commit statement
 echo -n "Commit statement: "
 read STATEMENT
 git commit -m "$STATEMENT" # must be in double-quotes because it will be multiple words long
-git push origin master
+
+# checking to see if a branch is specified
+if [ $# -eq 0 ] ; then
+	git push origin master
+else
+	git push origin $1
