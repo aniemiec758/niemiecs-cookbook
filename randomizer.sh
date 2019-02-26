c60fcd233aa167882fc31f498d1ae6a7f1159fb7
Anthony Niemiec <aniemiec758@gmail.com>
2019-02-26 13:03:30 -0500

    started randomizer.sh

---
+++ randomizer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# arguments checking
+MSG="mmeep"
+if [ $# -gt 0 ] ; then
+	MSG=$1
+fi
+
+# infinite color loop
+LOOPER=1
+while [ $LOOPER -le 5 ] ; do
+	# saving our current cursor position, to return to later
+	source ./getCursor.sh
+	OLDPOS=$CPOS
+
+	# moving the cursor to a completely random position
+	X=$(( RANDOM % 100 ))
+	Y=$(( RANDOM % 50 ))
+	echo -ne "\033["$Y";"$X"H"
+
+	# pick a random color and going with it
+	COLOR=$(( 30+$((RANDOM % 8)) ))
+	echo -ne "\033["$COLOR"$MSG"
+
+	# restoring our original cursor position
+	echo -ne "\033["$OLDPOS"H"
+
+#	(( LOOPER++ ))
+	sleep 0.05 # so the blink isn't too fast to perceive
+done
