c303d3cb357f1284b2dd858f1d5905f7eda635f7
Anthony Niemiec <aniemiec758@gmail.com>
2019-03-07 16:24:32 -0500

    modifying commit.sh

--- commit.sh
+++ commit.sh
@@ -2,22 +2,21 @@
 
 # because I'm lazy
 
-git add -A
-
-# getting the commit statement
+# getting the commit statement FIRST (user might ctrl-C if they change their mind)
 echo -n "Commit statement: "
 read STATEMENT
+git add -A
 git commit -m "$STATEMENT" # must be in double-quotes because it will be multiple words long
 
-# if there is no repote repo to push to
+# if the user doesn't want to push to remote
 if [ "$1" == "local" ] ; then
     echo -e "\033[32mCommitting to local repo ...\033[0m"
     exit
 fi
 
-# checking to see if a branch is specified
+# checking to see if a branch is specified, given that the user wants to push to remote
 if [ $# -eq 0 ] ; then
 	git push origin master
 else
-	git push origin "$1" # if the user does some weird input injection technique, I guess
+	git push origin "$1" # double-quoted in case the user somehow has a branch name that uses spaces, and they pass in two words as one argument??
 fi
